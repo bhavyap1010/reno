c1109eec03aab0c7abf441f67807538b5f4a93c8
Param Patel <101729325+Paramp24@users.noreply.github.com>
2025-04-23 13:45:39 -0400

    admin updated

--- client/admin.py
+++ client/admin.py
@@ -1,3 +1,62 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Profile, businessProfile, serviceRequest
+from .forms import SignUpForm
 
-# Register your models here.
+# Inline Profile inside User
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+# Extend the User Admin
+class UserAdmin(BaseUserAdmin):
+    add_form = SignUpForm
+    inlines = (ProfileInline,)
+
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+
+# BusinessProfile Admin
+class BusinessProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_user', 'service_location', 'display_services')
+
+    def get_user(self, obj):
+        return obj.user.username
+    get_user.short_description = 'User'
+
+    def display_services(self, obj):
+        return ", ".join(obj.services)
+    display_services.short_description = 'Services'
+
+# ServiceRequest Admin
+class ServiceRequestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_user', 'location', 'display_services')
+
+    def get_user(self, obj):
+        return obj.user.username
+    get_user.short_description = 'User'
+
+    def display_services(self, obj):
+        return ", ".join(obj.services_needed)
+    display_services.short_description = 'Services Needed'
+
+# Re-register User and register all models
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(businessProfile, BusinessProfileAdmin)
+admin.site.register(serviceRequest, ServiceRequestAdmin)
