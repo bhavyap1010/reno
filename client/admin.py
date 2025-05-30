2310bd8595bc680c0c268f38f292c46392afb35c
bhavyap1010 <bhavya.patel9@student.tdsb.on.ca>
2025-05-30 13:56:29 -0400

    base database

--- client/admin.py
+++ client/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import Profile, BusinessProfile, ServiceRequest, Review, Chatroom, Message
+from .models import Profile, BusinessProfile, ServiceRequest, Review, Chatroom, Message, ServiceRequestImage
 
 # Inline Profile inside User
 class ProfileInline(admin.StackedInline):
