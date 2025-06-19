9a9149aae8265fe559e3271ec0d304730fc57ca6
bhavyap1010 <bhavya.patel9@student.tdsb.on.ca>
2025-06-19 12:27:07 -0400

    clear

--- backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("service-requests/", views.ServiceRequestListCreate.as_view(), name="service-request-list"),
-    path("service-requests/delete/<int:pk>/", views.ServiceRequestDelete.as_view(), name="delete-service-request"),
-]
