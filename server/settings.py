b0b4b968f71e24c958fd4d53c099bf4422c71443
bhavyap1010 <bhavya.patel9@student.tdsb.on.ca>
2025-09-05 07:52:09 -0400

    added email verification

--- server/settings.py
+++ server/settings.py
@@ -73,7 +73,13 @@ ACCOUNT_LOGIN_METHODS = {'username', 'email'}
 ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory' # can be 'mandatory', 'optional', or 'none'
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
 ACCOUNT_FORMS = {
     'signup': 'client.forms.CustomSignupForm',
